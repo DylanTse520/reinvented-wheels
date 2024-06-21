fdb617cf114f53471bc1dae475ff9ff1e6b6114a
DylanTse520 <dylan052099@gmail.com>
2024-06-20 18:59:56 -0700

    chore: Update release.config.js to include necessary plugins for semantic-release

--- release.config.js
+++ release.config.js
@@ -3,18 +3,8 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-      },
-    ],
+    "@semantic-release/changelog",
+    "@semantic-release/git",
     "@semantic-release/github",
   ],
 };
