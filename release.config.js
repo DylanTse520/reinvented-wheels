7c2b41cbf1c1b8a50d48523db628fac21ee0419b
DylanTse520 <dylan052099@gmail.com>
2024-06-20 19:27:31 -0700

    fix: Update release.config.js to include necessary plugins for semantic release

--- release.config.js
+++ release.config.js
@@ -3,8 +3,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/git",
     "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 };
