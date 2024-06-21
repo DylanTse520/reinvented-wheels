45b370530821f72c35c4389235e7d13595993ff4
DylanTse520 <dylan052099@gmail.com>
2024-06-20 18:16:29 -0700

    chore: Update release.config.js for semantic-release
    This commit updates the release.config.js file to include the necessary plugins for semantic-release. It adds the "@semantic-release/changelog" plugin to generate a changelog file and the "@semantic-release/git" plugin to commit the changelog file. The changelogFile option is set to "CHANGELOG.md" and the assets option is set to include the changelog file. This change aligns with the recent user commits and follows the established convention in the repository.

--- release.config.js
+++ release.config.js
@@ -3,8 +3,18 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+      },
+    ],
     "@semantic-release/github",
-    "@semantic-release/git",
   ],
 };
